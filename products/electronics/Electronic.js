34341b2d88f5655b7d70b70db31930cb3ae5a85e
samsul alam <samsul7718@gmail.com>
2025-11-23 16:34:25 +0530

    electronic added in server product list

---
+++ products/electronics/Electronic.js
@@ -0,0 +1,44 @@
+import {lapi,speaker,watch,pend} from "../../assets";
+
+
+const  Electronic = [
+
+  
+  {
+    id: "601",
+    name: "Macbook pro",
+    price: 999.99,
+    description: "Worlds ist fastest and lightweighyt lapi",
+    images: [lapi,speaker,watch,pend],
+    colors: ["black","red", "blue"],
+    size: ["S", "M", "L", "XL"],
+  },
+  {
+    id: "602",
+    name: "Speaker",
+    price: 49.99,
+    description: "Enjoy your sound moment",
+    images: [speaker,watch,pend,lapi],
+    colors: ["red", "green"],
+    size: ["S", "M", "L", "XL"],
+  },
+   {
+    id: "603",
+    name: "Wrist Watch",
+    price: 55.99,
+    description: "Expensive watch ever",
+    images: [watch,pend,lapi,speaker],
+    colors: ["black", "green"],
+    size: ["S", "M", "L", "XL"],
+  },
+   {
+    id: "604",
+    name: "Pendrive-4.0a",
+    price: 32.99,
+    description: "Fast and quick transfer data",
+    images: [pend,lapi,speaker,watch],
+    colors: ["black","blue", "green"],
+    size: ["S", "M", "L", "XL"],
+  },
+];
+export {Electronic};
