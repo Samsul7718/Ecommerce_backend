917d301ad366b06baa9c0b33b3b4fb8bd1a94328
samsul alam <samsul7718@gmail.com>
2025-11-23 16:28:19 +0530

    men product added in server product

---
+++ products/men fashion/MenFashion.js
@@ -0,0 +1,43 @@
+const MenProducts = [
+  
+  {
+    id: "1",
+    name: "Formal Dress",
+    price: 79.99,
+    description: "These formals are designed for comfort and style...",
+    images: [formal,shirt,pant,T_shirt], 
+    colors: ["black","red", "blue"],
+    size: ["S", "M", "L", "XL"],
+    
+  },
+  {
+    id: "2",
+    name: "Casual Shirt",
+    price: 49.99,
+    description: "Soft cotton casual shirt for daily wear...",
+    images: [shirt,formal,T_shirt,pant],
+    colors: ["black","red", "blue"],
+    size: ["S", "M", "L", "XL"],
+    
+  },
+   {
+    id: "3",
+    name: "Casual Office Wear",
+    price: 39.99,
+    description: "Soft cotton casual shirt for office wear...",
+    images: [T_shirt,formal,pant,shirt],
+    colors: ["black","red", "blue"],
+    size: ["S", "M", "L", "XL"],
+  
+  },
+   {
+    id: "4",
+    name: "Casual Pant",
+    price: 36.99,
+    description: "Soft cotton casual T_shirt for daily wear...",
+    images: [pant,shirt,T_shirt,formal],
+    colors: ["black","red", "blue"],
+    size: ["S", "M", "L", "XL"],
+  },
+];
+export {MenProducts};
