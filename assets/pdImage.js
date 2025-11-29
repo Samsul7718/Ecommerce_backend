88e49fa46a4bd8d6b0a16a770225e0ceb0845d16
samsul alam <samsul7718@gmail.com>
2025-11-29 19:17:14 +0530

    pdimage fro gym added

--- assets/pdImage.js
+++ assets/pdImage.js
@@ -9,7 +9,7 @@ export default{
    kurti:"/images/women-clth/kurti.jpeg",
    saree:"/images/women-clth/saree.webp",
    burkha:"/images/women-clth/burkha.png",
-   chest:"/images/gym/chest.avif",
+   chest:"/images/gym/cheast.avif",
    dumble:"/images/gym/dumble.jpg",
    starx:"/images/gym/starx.webp",
    trademill:"/images/gym/trademill.webp",
